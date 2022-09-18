29aeddf9f38a65fb8ac8ffac665cdbd05dc4c657
Aracomell <109452970+Aracomell@users.noreply.github.com>
2022-09-18 16:23:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -35,13 +35,13 @@ global.APIKeys = { // APIKey Here
 }
 
 global.linkyt = 'https://youtu.be/60yZO4UQKdI'
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷'
-global.linkgc = 'https://chat.whatsapp.com/Kyz397uqvJZ5AvX7I4BSID'
+global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝙰𝚛𝚛𝚛.𝚝𝚑𝚎.𝚌𝚗𝚝𝚚𝚚'
+global.linkgc = 'https://chat.whatsapp.com/KNkpoT4oX5yFA18GlPttgl'
 global.linkig = 'https://www.instagram.com/the.sad.boy01'
 global.deslink = ''
 global.logo = 'https://telegra.ph/file/1665521d176fbf172d735.jpg'
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@the.sad.boy01'
+global.bodlink = '@𝙰𝚛𝚛𝚛.𝚝𝚑𝚎.𝚌𝚗𝚝𝚚𝚚'
 
 global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
 global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
@@ -52,7 +52,7 @@ global.urlnya = global.linkgc
 
 //============= callButtons ============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-3362-82144'
+global.phn = '+62 895-0423-1588'
 
 //========== Tampilan Bot =============//
 global.sa = '╭─'
